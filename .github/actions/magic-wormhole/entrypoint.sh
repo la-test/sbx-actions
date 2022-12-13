34df5a9d2aac1a21ef2590b6b8b59636f3591c12
Benoit Donneaux <benoit@leastauthority.com>
2022-12-13 21:22:37 +0100

    Try to catch stderr and out
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole/entrypoint.sh
+++ .github/actions/magic-wormhole/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 
+echo -n 'out=' >> $GITHUB_OUTPUT
+wormhole "${@}" >> $GITHUB_OUTPUT 2> stderr.tmp
 echo -n 'err=' >> $GITHUB_OUTPUT
-wormhole "${@}" 2>> $GITHUB_OUTPUT
+cat stderr.tmp >> $GITHUB_OUTPUT
