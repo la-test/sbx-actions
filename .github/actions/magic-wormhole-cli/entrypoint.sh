2fbca8a643facd363917908f1ceabed71d21f4b6
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 09:25:13 +0100

    Quote outputs and use step summary
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-echo -n 'out=' >> $GITHUB_OUTPUT
-wormhole "${@}" >> $GITHUB_OUTPUT 2> stderr.tmp
-echo >> $GITHUB_OUTPUT
-echo -n 'err=' >> $GITHUB_OUTPUT
-cat stderr.tmp >> $GITHUB_OUTPUT
+echo -n 'out="' >> $GITHUB_OUTPUT
+wormhole "${@}" >> $GITHUB_OUTPUT 2> $GITHUB_STEP_SUMMARY
+echo '"' >> $GITHUB_OUTPUT
+echo -n 'err="$(tail -1 $GITHUB_STEP_SUMMARY)"' >> $GITHUB_OUTPUT
