f8cf589c5a9cb48bb603d4bc52ffde61ed5150ec
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 14:22:03 +0100

    Still not working
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -14,14 +14,14 @@ TMP_ERR="err.tmp"
 # Call wormhole with all arguments
 wormhole "${@}" > "${TMP_OUT}" 2> "${TMP_ERR}"
 
-OUT=$(tail -1 "${TMP_OUT}" | tr -d '\n')
-ERR=$(tail -1 "${TMP_ERR}" | tr -d '\n')
+OUT=$(cat "${TMP_OUT}")
+ERR=$(tail -1 "${TMP_ERR}")
 
 # Pass output named out from stdout
-echo "out='${OUT}'" >> $GITHUB_OUTPUT
+echo "out=${OUT}" >> $GITHUB_OUTPUT
 
 # Pass output named err from last line of stderr
-echo "err='${ERR}'" >> $GITHUB_OUTPUT
+echo "err=${ERR}" >> $GITHUB_OUTPUT
 
 # Preapre step summary from stderr
 cat ${TMP_ERR} >> $GITHUB_STEP_SUMMARY
