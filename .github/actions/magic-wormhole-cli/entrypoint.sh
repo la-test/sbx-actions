4dbad99339f0ef7ae5526e78345721f0bd61758e
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 14:04:19 +0100

    Try to make a long string as output
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -14,14 +14,14 @@ TMP_ERR="err.tmp"
 # Call wormhole with all arguments
 wormhole "${@}" > "${TMP_OUT}" 2> "${TMP_ERR}"
 
-OUT=$(cat "${TMP_OUT}" | tr -d '\n' | tr -d "'")
-ERR=$(cat "${TMP_ERR}" | tr -d '\n' | tr -d "'")
+OUT=$(cat "${TMP_OUT}" | tr -d '\n' | tr -d "'" | tr ' ' '_')
+ERR=$(cat "${TMP_ERR}" | tr -d '\n' | tr -d "'" | tr ' ' '_')
 
 # Pass output named out from stdout
-echo "out=\'${OUT}\'" >> $GITHUB_OUTPUT
+echo "out='${OUT}'" >> $GITHUB_OUTPUT
 
 # Pass output named err from last line of stderr
-echo "err=\'${ERR}\'" >> $GITHUB_OUTPUT
+echo "err='${ERR}'" >> $GITHUB_OUTPUT
 
 # Preapre step summary from stderr
 cat ${TMP_ERR} >> $GITHUB_STEP_SUMMARY
