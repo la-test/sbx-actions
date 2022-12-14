56dcdee411a8da7f480debd80161a313411bdfec
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 09:38:24 +0100

    Avoid quotes and newlines in outputs
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-echo -n 'out="' >> $GITHUB_OUTPUT
-wormhole "${@}" >> $GITHUB_OUTPUT 2> $GITHUB_STEP_SUMMARY
-echo '"' >> $GITHUB_OUTPUT
-echo -n 'err="$(tail -1 $GITHUB_STEP_SUMMARY)"' >> $GITHUB_OUTPUT
+echo -n 'out=' >> $GITHUB_OUTPUT
+wormhole "${@}" 2> $GITHUB_STEP_SUMMARY | tr -d '\n' >> $GITHUB_OUTPUT
+echo >> $GITHUB_OUTPUT
+echo -n 'err=' >> $GITHUB_OUTPUT
+tail -1 $GITHUB_STEP_SUMMARY | tr -d '\n' >> $GITHUB_OUTPUT
+echo >> $GITHUB_OUTPUT
