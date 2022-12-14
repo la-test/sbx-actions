445fc1e50ac80771eeb7b1a3e3ea7f9ea632ddd3
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 14:37:52 +0100

    Use delimeter for multiline output
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -3,25 +3,22 @@
 set -o xtrace
 
 # Prepare temporary files
-# TMP_OUT="$(mktemp --tmpdir $(basename $0)_out.XXXXXXXXXX)"
-# TMP_ERR="$(mktemp --tmpdir $(basename $0)_err.XXXXXXXXXX)"
-TMP_OUT="out.tmp"
-TMP_ERR="err.tmp"
+TMP_OUT="$(mktemp --tmpdir $(basename $0)_out.XXXXXXXXXX)"
+TMP_ERR="$(mktemp --tmpdir $(basename $0)_err.XXXXXXXXXX)"
 
 # Make sure they will be deleted
-# trap "rm -f ${TMP_OUT} ${TMP_ERR}" EXIT
+trap "rm -f ${TMP_OUT} ${TMP_ERR}" EXIT
 
 # Call wormhole with all arguments
 wormhole "${@}" > "${TMP_OUT}" 2> "${TMP_ERR}"
 
-OUT=$(tail -2 "${TMP_OUT}" | head -1)
-ERR=$(cat "${TMP_ERR}")
-
 # Pass output named out from stdout
-echo "out=${OUT}" >> $GITHUB_OUTPUT
+echo "out=$(cat "${TMP_OUT}")" >> $GITHUB_OUTPUT
 
 # Pass output named err from last line of stderr
-echo "err=${ERR}" >> $GITHUB_OUTPUT
+echo "err<<$(basename ${TMP_ERR})" >> $GITHUB_OUTPUT
+cat "${TMP_ERR}" >> $GITHUB_OUTPUT
+echo "$(basename ${TMP_ERR})" >> $GITHUB_OUTPUT
 
 # Preapre step summary from stderr
 cat ${TMP_ERR} >> $GITHUB_STEP_SUMMARY
