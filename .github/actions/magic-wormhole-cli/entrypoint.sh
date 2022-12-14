7d0c422168b49eba991f0b72f12f89f31de09455
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 10:48:29 +0100

    Avoid deletion for debug
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -3,11 +3,13 @@
 set -o xtrace
 
 # Prepare temporary files
-TMP_OUT="$(mktemp --tmpdir $(basename $0)_out.XXXXXXXXXX)"
-TMP_ERR="$(mktemp --tmpdir $(basename $0)_err.XXXXXXXXXX)"
+# TMP_OUT="$(mktemp --tmpdir $(basename $0)_out.XXXXXXXXXX)"
+# TMP_ERR="$(mktemp --tmpdir $(basename $0)_err.XXXXXXXXXX)"
+TMP_OUT="out.tmp"
+TMP_ERR="err.tmp"
 
 # Make sure they will be deleted
-trap "rm -f ${TMP_OUT} ${TMP_ERR}" EXIT
+# trap "rm -f ${TMP_OUT} ${TMP_ERR}" EXIT
 
 # Call wormhole with all arguments
 wormhole "${@}" > ${TMP_OUT} 2> ${TMP_ERR}
