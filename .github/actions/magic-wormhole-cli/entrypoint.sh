3dd5d75fad1e1ed27fc605ff4599252294e8702b
Benoit Donneaux <benoit@leastauthority.com>
2022-12-14 10:53:15 +0100

    Avoid err output to test
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole-cli/entrypoint.sh
+++ .github/actions/magic-wormhole-cli/entrypoint.sh
@@ -17,12 +17,12 @@ wormhole "${@}" > ${TMP_OUT} 2> ${TMP_ERR}
 # Prepare output named out from stdout
 echo -n 'out=' >> $GITHUB_OUTPUT
 cat ${TMP_OUT} | tr -d '\n' >> $GITHUB_OUTPUT
-echo >> $GITHUB_OUTPUT
+#echo >> $GITHUB_OUTPUT
 
 # Prepare output named err from last line of stderr
-echo -n 'err=' >> $GITHUB_OUTPUT
-tail -1 ${TMP_ERR} | tr -d '\n' >> $GITHUB_OUTPUT
-echo >> $GITHUB_OUTPUT
+#echo -n 'err=' >> $GITHUB_OUTPUT
+#tail -1 ${TMP_ERR} | tr -d '\n' >> $GITHUB_OUTPUT
+#echo >> $GITHUB_OUTPUT
 
 # Preapre step summary from stderr
 cat ${TMP_ERR} >> $GITHUB_STEP_SUMMARY
