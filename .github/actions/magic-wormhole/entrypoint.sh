cf103766288a594d2191d5f89b3bd16b30fe91d4
Benoit Donneaux <benoit@leastauthority.com>
2022-12-15 13:06:38 +0100

    Swith to new action using public wormhole image
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- .github/actions/magic-wormhole/entrypoint.sh
+++ .github/actions/magic-wormhole/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash
 
 # Prepare temporary files
 TMP_OUT="$(mktemp --tmpdir $(basename $0)_out.XXXXXXXXXX)"
@@ -8,17 +8,37 @@ TMP_ERR="$(mktemp --tmpdir $(basename $0)_err.XXXXXXXXXX)"
 trap "rm -f ${TMP_OUT} ${TMP_ERR}" EXIT
 
 # Call wormhole with all arguments
-wormhole "${@}" > "${TMP_OUT}" 2> "${TMP_ERR}"
+wormhole "${@}" > "${TMP_OUT}" 2> "${TMP_ERR}" && RET=0 || RET=1
 
-# Pass text received from stdout
-echo "text<<$(basename "${TMP_OUT}")" >> $GITHUB_OUTPUT
-cat "${TMP_OUT}" >> $GITHUB_OUTPUT
-echo "$(basename ${TMP_OUT})" >> $GITHUB_OUTPUT
+# Lookup output-file in arguments
+OUTPUT_FILE=''
+ARGS=("${@}")
+for i in "${!ARGS[@]}"; do
+  echo $i
+  if [[ "\\${ARGS[$i]}" =~ \\(--output-file|-o) ]]; then
+    OUTPUT_FILE="${ARGS[$((i+1))]}"
+  fi
+done
+
+# TODO: Something is wrong if FILE is still empty
+
+# If output-file does not exist, use stdout to create it
+if [ ! -s "${OUTPUT_FILE}" ]; then
+  cp -a "${TMP_OUT}" "${OUTPUT_FILE}"
+fi
+
+# Append some info based on the exit code in result and summary
+if [ $RET -eq 0 ]; then
+  echo "SUCCESS - data has been transfered" >> "${TMP_ERR}"
+  echo ":heavy_check_mark: Wormhole succeeded: data has been transfered" >> $GITHUB_STEP_SUMMARY
+else
+  echo "FAILURE - data has NOT been transfered" >> "${TMP_ERR}"
+  echo ":x: Wormhole failed: data has NOT been transfered" >> $GITHUB_STEP_SUMMARY
+fi
 
 # Pass stderr as result
 echo "result<<$(basename "${TMP_ERR}")" >> $GITHUB_OUTPUT
 cat "${TMP_ERR}" >> $GITHUB_OUTPUT
 echo "$(basename ${TMP_ERR})" >> $GITHUB_OUTPUT
 
-# Pass stderr as step summary too
-cat ${TMP_ERR} >> $GITHUB_STEP_SUMMARY
+exit ${RET}
